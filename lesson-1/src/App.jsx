9e2ca2bea61486f3f15d7d61eb3c3fe43f131275
Rakhmatovdev <rakhmatovjasur3@gmail.com>
2024-01-14 20:20:28 +0500

    React&Vite

---
+++ lesson-1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Jasur</h1>
+      <h3>Go to project</h3>
+    </>
+  )
+}
+
+export default App
