7c6707b8b4d26b98ac9986939da6eaced9f6a198
Rakhmatovdev <rakhmatovjasur3@gmail.com>
2024-01-28 21:13:49 +0500

    Yangi darslar qo'shildi React-router-dom

---
+++ lesson-41A useFerch custom hook/my-app/src/components/TripsList.jsx
@@ -0,0 +1,32 @@
+// style
+import "./TripList.css";
+
+import { useState } from "react";
+import { useFetch } from '../hooks/useFetch'
+
+function TripList() {
+  const [url, setUrl] = useState("http://localhost:3000/trips")
+  const { data: trips } = useFetch(url)
+
+  return (
+    <div className="trip-list">
+      <h1>TripList</h1>
+      <ul>
+        {trips && trips.map((trip) => {
+          return (
+            <li key={trip.title}>
+              <h2>{trip.title}</h2>
+              <p>{trip.price}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="filters">
+        <button onClick={() => setUrl('http://localhost:3000/trips?loc=Europe')}>European Trips</button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>All Trips</button>
+      </div>
+    </div>
+  );
+}
+
+export default TripList;
---
+++ lesson-42A Loading (isPendinding)/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import TripsList from './components/TripsList'
+
+function App() {
+
+  return (
+   <>
+   <TripsList/>
+   </>
+  )
+}
+
+export default App
